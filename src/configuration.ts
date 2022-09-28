db69dc926a811dfb20acca137f97ed8957cb9dd4
weiyuang <weiyuang@joyy.com>
2022-09-28 19:33:08 +0800

    initia;

--- src/configuration.ts
+++ src/configuration.ts
@@ -3,9 +3,10 @@ import { ILifeCycle } from '@midwayjs/core';
 import { Application } from 'egg';
 import { join } from 'path';
 import * as egg from '@midwayjs/web';
+import * as task from '@midwayjs/task';
 
 @Configuration({
-  imports: [egg],
+  imports: [egg, task],
   importConfigs: [join(__dirname, './config')],
 })
 export class ContainerLifeCycle implements ILifeCycle {
