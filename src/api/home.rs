0e323ecfa8398729e868a7d6d9929499a1223c20
weiyuang <weiyuang@joyy.com>
2022-10-02 16:05:36 +0800

    inital api

--- src/api/home.rs
+++ src/api/home.rs
@@ -1,6 +1,16 @@
+use rocket::serde::{Serialize, json::Json};
+
+use crate::api::*;
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct HomeResp {
+  string: String,
+}
+
 #[get("/")]
-fn home() {
-  
+fn home() -> Json<ApiResponse<HomeResp>> {
+  api_success(HomeResp { string: String::from("ok") })
 }
 
 pub fn routes() -> Vec<rocket::Route> {
--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,6 +1,24 @@
-use rocket::{Rocket, Build};
+use rocket::{Rocket, Build, response};
+use rocket::serde::{Serialize, json::Json};
 
 pub mod home;
+
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct ApiResponse<T> {
+  code: i32,
+  message: String,
+  data: T,
+}
+
+pub fn api_success<T>(data: T) -> Json<ApiResponse<T>> {
+  Json(ApiResponse {
+     code: 200, 
+     message: String::from("OK"), 
+     data,
+  })
+}
 
 
 pub fn init() -> Rocket<Build> {
